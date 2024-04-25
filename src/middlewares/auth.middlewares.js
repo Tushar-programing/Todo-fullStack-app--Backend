8ece9c3d6aeef877e9b2c7b7a5f3cc54f7c2e8ba
Tushar saini <ttushar476@gmail.com>
2024-04-25 20:33:16 +0530

    7 th coommit

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -5,28 +5,28 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT =  asyncHandler( async(req, _, next) => {
     try {
-        // console.log("1");
+        console.log("1");
 
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        // console.log("1.1");
+        console.log("1.1");
         if (!token) {
             throw new ApiError(400, "Unauthorized request")
         }
-        // console.log("1.2");
+        console.log("1.2");
       
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-        // console.log("1.3");
+        console.log("1.3");
 
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-        // console.log("1.4");
+        console.log("1.4");
 
         if (!user) {
             throw new ApiError(400, "Invalid access token")
         }
-        // console.log("1.5");
+        console.log("1.5");
 
         req.user = user
-        // console.log("1.6");
+        console.log("1.6");
 
         next()
 
