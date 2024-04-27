cbaafd34059db42e0aeb577f493bc59545633f9f
Tushar saini <ttushar476@gmail.com>
2024-04-27 18:47:21 +0530

    tenth commit

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -125,7 +125,8 @@ const logoutUser = asyncHandler(async(req, res) => {
 
     const options = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite: 'none',
     }
 
     return res.status(200)
